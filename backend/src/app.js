7a9aec2c4d3ef5a2919c3aa4d35f5488d37fb8df
Dr Yogesh Bhardwaj <kbsjyotish@gmail.com>
2026-03-07 15:41:40 +0530

    Disable strict origin validation for origin dev testing bypass override

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,9 +22,7 @@ const { errorResponse } = require('./utils/response');
 const app = express();
 
 app.use((req, res, next) => {
-    const origin = req.headers.origin;
-
-    res.setHeader("Access-Control-Allow-Origin", origin || "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
